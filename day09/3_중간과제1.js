63d2c51aa8ec1d4370765ec69cb0ab7b1eab97cc
riwon-sys <le_labo@kakao.com>
2024-11-21 13:58:10 +0900

    lelabo

---
+++ day09/3_중간과제1.js
@@ -0,0 +1,26 @@
+
+let name = "유재석"
+let name2 = "강호동"
+
+let point1 = '아이스커피'
+let
+
+let "pointArray" = [ '아이스커피' , '뜨거운커피' , '스무디' ]
+
+
+let 유재석 = [ ];
+let 강호동 = [ ];
+
+유재석.push( 'ice1' , 'ice2' );
+let wait =['유재석'];
+
+console.log(wait.indexOf('유재석'));
+
+강호동.push( 'hot' , 'app' );
+
+wait.push('강호동')
+
+console.log(wait.indexOf('강호동'));
+
+wait.slice( 0 , 1 );
+wait.slice( 0 , 1 );
